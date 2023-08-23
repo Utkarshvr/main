f12f3cfe26d2870dd20dc6f906403e77550d286a
Utkarshvr <utkarshv995@gmail.com>
2023-08-23 21:46:31 +0530

    Shifted to Next v13

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,64 @@
+import Link from "next/link";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
+
+import { heroText, heroTitle } from "../../constants/constants";
+
+const Hero = (props) => (
+  <>
+    <Section row nopadding>
+      <LeftSection style={{ display: "flex" }} className="relative">
+        <div>
+          <SectionTitle main center>
+            <ReactMarkdown className="child-p-mb-0">{heroTitle}</ReactMarkdown>
+          </SectionTitle>
+          <SectionText>
+            <ReactMarkdown>{heroText}</ReactMarkdown>
+          </SectionText>
+
+          <Link href="#about">
+            <Button onClick={props.handleClick}>Learn More</Button>
+          </Link>
+        </div>
+
+        <div style={{ position: "absolute", top: 0, right: 0, zIndex: -999 }}>
+          <div
+            style={{ position: "relative", width: "180px", height: "180px" }}
+          >
+            <img
+              src="/images/profile.jpg"
+              style={{
+                borderRadius: "100%",
+                border: "2px solid #fff",
+                width: "100%",
+                height: "100%",
+                objectFit: "cover", // Ensures the image covers the entire container
+              }}
+              alt="Profile"
+            />
+            <div
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+                background: "rgba(0,0,0,0.4)",
+                borderRadius: "100%",
+              }}
+            ></div>
+          </div>
+        </div>
+      </LeftSection>
+    </Section>
+  </>
+);
+
+export default Hero;
